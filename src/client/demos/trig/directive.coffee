83b660667cf45062ba2b4c67404afb6ff3876057
David Souther <davidsouther@gmail.com>
2014-10-06 12:11:53 -0400

    Added scaleSvc

--- src/client/demos/trig/directive.coffee
+++ src/client/demos/trig/directive.coffee
@@ -11,8 +11,22 @@ angular.module('graphing.demos.trig', [
 .directive 'trigDemo', ->
     restrict: 'E'
     templateUrl: 'demos/trig'
-.controller 'TrigCtrl', ($scope)->
+.controller 'TrigCtrl', ($scope, ScaleSvc)->
     $scope.data = []
+
+    $scope.x = { min: -1.2, max: 5 };
+    $scope.y = {};
+    $scope.y.max = (260 * (x.max - x.min)) * (1 / (2 * 760));
+    $scope.y.min = -y.max;
+    $scope.time = 0;
+    $scope.delta = .015 / 2;
+    $scope.value = {x: Math.PI, y: 0};
+
+    $scope.scales = ScaleSvc({
+        x: ['linear', $scope.x.min, $scope.x.max],
+        y: ['linear', $scope.y.min, $scope.y.max]
+    });
+
     i = 0
     while i < 84
         $scope.data.push i * 10 / 84
--- src/client/scales/main.js
+++ src/client/scales/main.js
@@ -47,6 +47,60 @@ angular.module('graphing.scales', [
     $rootScope.TWO_PI = 2 * Math.PI;
     $rootScope.Math = Math;
 })
+.service('ScaleSvc', function(){
+    var getScales = function getScales(scales, $element, options){
+        var e = $element[0];
+        var margins = (options || {}).margins || {
+            top: 10,
+            right: 10,
+            bottom: 30,
+            left: 30
+        };
+
+        margins.leftRight = margins.left + margins.right;
+        margins.topBottom = margins.top + margins.bottom;
+
+        // Get the bounds of the parent element
+        var height = (
+            e.offsetHeight || e.clientHeight
+        ) - margins.leftRight;
+        var width = (
+            e.offsetWidth || e.clientWidth
+        ) - margins.topBottom;
+
+        // Reset the scales
+        var $scales = {};
+
+        // Evaluate the expression to get the scale params
+
+        for (var name in scales) { // Iterate the object
+            var scale = scales[name];
+            // The scale object is an array
+            var type = scale.shift();
+            var maxima = 0, minima = 0;
+            if (name === 'x' || name === 'y'){
+                maxima = {
+                    x: width,
+                    y: height
+                }[name] || scale[2] || ((width + height) / 2);
+                minima = {
+                    x: margins.left,
+                    y: margins.top
+                }[name];
+            } else {
+                minima = scale[2];
+                maxima = scale[3];
+            }
+            $scales[name] = Scales[type](
+                scale[0], scale[1], minima, maxima
+            );
+        }
+
+        return $scales;
+    };
+
+    return {getScales: getScales};
+})
 .filter('scale', function(){
     return function Scale(value, scale){
         if(angular.isFunction(scale)){
@@ -56,60 +110,19 @@ angular.module('graphing.scales', [
         }
     };
 })
-.directive('graphScales', function($parse){
+.directive('graphScales', function($parse, ScaleSvc){
     return {
         priority: 400,
         compile: function(){
             return {
                 pre: function($scope, $element, attrs){
-                    var e = $element[0];
                     var $exp = $parse(attrs.graphScales);
                     var setScales = (function setScales(){
-                        var margins = ($scope.$chartOptions || {}).margins || {
-                            top: 10,
-                            right: 10,
-                            bottom: 30,
-                            left: 30
-                        };
-
-                        margins.leftRight = margins.left + margins.right;
-                        margins.topBottom = margins.top + margins.bottom;
-
-                        // Get the bounds of the parent element
-                        var height = (
-                            e.offsetHeight || e.clientHeight
-                        ) - margins.leftRight;
-                        var width = (
-                            e.offsetWidth || e.clientWidth
-                        ) - margins.topBottom;
-
-                        // Reset the scales
-                        $scope.$scales = {};
-
-                        // Evaluate the expression to get the scale params
-                        var scales = $exp($scope);
-                        for (var name in scales) { // Iterate the object
-                            var scale = scales[name];
-                            // The scale object is an array
-                            var type = scale.shift();
-                            var maxima = 0, minima = 0;
-                            if (name === 'x' || name === 'y'){
-                                maxima = {
-                                    x: width,
-                                    y: height
-                                }[name] || scale[2] || ((width + height) / 2);
-                                minima = {
-                                    x: margins.left,
-                                    y: margins.top
-                                }[name];
-                            } else {
-                                minima = scale[2];
-                                maxima = scale[3];
-                            }
-                            $scope.$scales[name] = Scales[type](
-                                scale[0], scale[1], minima, maxima
-                            );
-                        }
+                        $scope.$scales = ScaleSvc.getScales(
+                            $exp($scope),
+                            $element,
+                            $scope.$chartOptions
+                        );
                         return arguments.callee;
                     }());
                     $scope.$on('Window Resized', setScales);
